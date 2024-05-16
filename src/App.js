25ffe10155c6a79631ca7ad66e7d67c9160eb01a
Harminn <harminhshah@gmail.com>
2024-05-16 18:18:58 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <h1 className="text-4xl font-bold text-red-600 underline uppercase">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-4xl flex justify-center font-bold text-red-600 p-5 opacity-60 my-8 underline uppercase">
+        Hello world!
+      </h1>
+      <div class="flex flex-column justify-between">
+        <div class="basis-1/4 sm:basis-1/3">01</div>
+        <div class="basis-1/4 md:basis-1/3">02</div>
+        <div class="basis-1/4 xl:basis-1/3">03</div>
+      </div>
+    </>
   );
 }
 
