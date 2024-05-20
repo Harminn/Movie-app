1b3e5c7dec1cf5cedc6cfbd4aa726700d00e73c7
Harminn <harminhshah@gmail.com>
2024-05-20 18:15:23 +0530

    latest commit 20 may

--- src/App.js
+++ src/App.js
@@ -14,15 +14,15 @@ const App = () => {
   return (
     <>
       <div className="min-h-screen bg-gray-100">
-        <header className="flex flex-row bg-slate-950 py-4">
-          <div className="container mx-auto flex flex-row ml-16">
+        <header className="fixed top-0 left-0 right-0 flex flex-row bg-slate-900 py-2 z-10">
+          <div className="container mx-auto flex justify-center align-center ml-12">
             <h1 className="text-4xl text-red-600 font-bold ">Tudoom</h1>
-            <BiSolidCameraMovie className="h-9 w-10 text-red-500 pl-2" />
+            <BiSolidCameraMovie className="h-9 w-10 text-red-500 pl-2 pt-2" />
           </div>
-          <div className="flex gap-6 justify-end mt-0 mr-16">
+          {/* <div className="flex gap-6 justify-end mt-0 mr-16">
             <IoSearch className="h-9 w-10 text-red-600" />
             <IoOptionsOutline className="text-red-600 h-9 w-10" />
-          </div>
+          </div> */}
         </header>
 
         <main className="bg-slate-900 m-0 py-16">
@@ -32,11 +32,11 @@ const App = () => {
               placeholder="Search movies..."
               value={searchQuery}
               onChange={handleSearchChange}
-              className="px-4 py-2 bg-gray-600 border border-gray-300 rounded-l-md focus:outline-none"
+              className="px-4 py-2 bg-gray-600 border border-gray-300 rounded-l-md rounded-r-md focus:outline-none"
             />
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-r-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-gray-900">
+            {/* <button className="bg-blue-500 text-white px-4 py-2 rounded-r-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-gray-900">
               Search
-            </button>
+            </button> */}
           </div>
 
           {/* <MovieList searchQuery={searchQuery} /> */}
--- src/MovieDetails.jsx
+++ src/MovieDetails.jsx
@@ -1,34 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import ReactPaginate from "react-paginate";
+import { useImmer } from "use-immer";
 
 const MovieDetails = ({ searchQuery }) => {
   const [data, setData] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
   const navigate = useNavigate();
 
-  const handlePrevPage = () => {
-    setCurrentPage((prevPage) => prevPage - 1);
-  };
-
-  const handleNextPage = () => {
-    setCurrentPage((prevPage) => prevPage + 1);
-  };
-
-  // const fetchData = () => {
-  //   fetch("https://dummyapi.online/api/movies")
-  //     .then((response) => response.json())
-  //     .then((jsonData) => setData(jsonData))
-  //     .catch((error) => console.error("Error fetching data:", error));
-  // };
-
-  const fetchData = async (page) => {
+  const fetchData = async () => {
     const url =
       "https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmetal&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc";
     const options = {
       method: "GET",
       headers: {
-        "X-RapidAPI-Key": "278de21abdmshf13b4533e2b1d20p198a3bjsn079762c16e28",
+        "X-RapidAPI-Key": "f6c7d07c18mshfa1f06c1640ff0dp12a09ejsn4b39844f7f4b",
         "X-RapidAPI-Host": "anime-db.p.rapidapi.com",
       },
     };
@@ -44,8 +29,8 @@ const MovieDetails = ({ searchQuery }) => {
   };
 
   useEffect(() => {
-    fetchData(currentPage);
-  }, [currentPage]);
+    fetchData();
+  }, []);
 
   console.log(data);
 
@@ -57,9 +42,9 @@ const MovieDetails = ({ searchQuery }) => {
     <>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-16 ">
         {data
-          // .filter((movieData) =>
-          //   movieData.movie.toLowerCase().includes(searchQuery.toLowerCase())
-          // )
+          .filter((movieData) =>
+            movieData.title.toLowerCase().includes(searchQuery.toLowerCase())
+          )
           .map((movieData) => (
             <div
               onClick={() => handleMovieClick(movieData.link)}
@@ -78,26 +63,6 @@ const MovieDetails = ({ searchQuery }) => {
               </div>
             </div>
           ))}
-      </div>
-
-      <div className="flex justify-center mt-4">
-        <button
-          className="px-4 py-2 bg-blue-500 text-white rounded-l-md hover:cursor-pointer hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-gray-900"
-          onClick={handlePrevPage}
-          disabled={currentPage === 1}
-        >
-          Previous
-        </button>
-        <span className="px-4 py-2 bg-gray-200 text-gray-700">
-          Page {currentPage}
-        </span>
-        <button
-          className="px-4 py-2 bg-blue-500 text-white rounded-r-md hover:cursor-pointer hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-gray-900"
-          onClick={handleNextPage}
-          disabled={data.length < 10} // Disable next button if less than 10 items
-        >
-          Next
-        </button>
       </div>
     </>
   );
